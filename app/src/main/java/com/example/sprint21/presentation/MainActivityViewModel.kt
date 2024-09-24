b68852e9fb00722df62355eca887b5ec6535c680
Mike Wasser <m.wasser@agimagroup.ru>
2024-09-24 16:06:42 +0300

    Add ViewModel

--- app/src/main/java/com/example/sprint21/presentation/MainActivityViewModel.kt
+++ app/src/main/java/com/example/sprint21/presentation/MainActivityViewModel.kt
@@ -2,6 +2,7 @@ package com.example.sprint21.presentation
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.sprint21.domain.model.Track
 import com.example.sprint21.domain.usecase.GetTracksUseCase
 import kotlinx.coroutines.CoroutineScope
@@ -13,24 +14,13 @@ import java.io.Closeable
 import kotlin.coroutines.CoroutineContext
 
 class MainActivityViewModel(
-    private val getTracksUseCase: GetTracksUseCase,
-    private val customScope: CloseableCoroutineScope = CloseableCoroutineScope()
-) :
-    ViewModel(customScope) {
+    private val getTracksUseCase: GetTracksUseCase
+) : ViewModel() {
     val tracksData = MutableLiveData<List<Track>>()
 
     fun getTracks() {
-        customScope.launch {
+        viewModelScope.launch {
             tracksData.postValue(getTracksUseCase())
         }
-    }
-}
-
-class CloseableCoroutineScope(
-    context: CoroutineContext = SupervisorJob() + Dispatchers.Main.immediate
-) : Closeable, CoroutineScope {
-    override val coroutineContext: CoroutineContext = context
-    override fun close() {
-        coroutineContext.cancel()
     }
 }
